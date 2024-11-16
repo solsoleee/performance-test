69602951d705c3e04af37ebc5df93dfc0c191a99
solsoleee <wsw3727@gmail.com>
2024-11-16 16:50:43 +0900

    test

--- locustfile.py
+++ locustfile.py
@@ -1,31 +1,31 @@
-# from locust import HttpUser, task, between
-# import random
+from locust import HttpUser, task, between
+import random
 
-# class FlaskTestUser(HttpUser):
-#     wait_time = between(1, 2)  # 각 요청 사이의 대기 시간 (초)
+class FlaskTestUser(HttpUser):
+    wait_time = between(1, 2)  # 각 요청 사이의 대기 시간 (초)
 
-#     # 테스트 데이터 준비
-#     titles = ["Breaking News", "Sports Update", "Tech Innovations"]
-#     contents = [
-#         "The latest news about the economy.",
-#         "Highlights from yesterday's match.",
-#         "AI and ML are transforming industries."
-#     ]
+    # 테스트 데이터 준비
+    titles = ["Breaking News", "Sports Update", "Tech Innovations"]
+    contents = [
+        "The latest news about the economy.",
+        "Highlights from yesterday's match.",
+        "AI and ML are transforming industries."
+    ]
 
-#     @task
-#     def test_predict(self):
-#         # 랜덤 데이터 생성
-#         title = random.choice(self.titles)
-#         content = random.choice(self.contents)
+    @task
+    def test_predict(self):
+        # 랜덤 데이터 생성
+        title = random.choice(self.titles)
+        content = random.choice(self.contents)
 
-#         # /predict 엔드포인트 요청
-#         response = self.client.post(
-#             "/predict",
-#             data={"title": title, "content": content}
-#         )
+        # /predict 엔드포인트 요청
+        response = self.client.post(
+            "/predict",
+            data={"title": title, "content": content}
+        )
         
-#         # 로깅
-#         if response.status_code == 200:
-#             print(f"Request successful: {response.json()}")
-#         else:
-#             print(f"Request failed with status code: {response.status_code}")
+        # 로깅
+        if response.status_code == 200:
+            print(f"Request successful: {response.json()}")
+        else:
+            print(f"Request failed with status code: {response.status_code}")
